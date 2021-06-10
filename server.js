2ef7ee791f5d93c7a9cc9a5677afe6af4f12445c
neha-s25 <neha.s25@gmail.com>
2021-06-10 16:22:29 -0700

    Initial setup

---
+++ server.js
@@ -0,0 +1,18 @@
+// imports
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const port = process.env.PORT || 4000;
+const app = express();
+
+// TODO: middleware - CORS
+app.use(cors());
+
+// middleware - JSON parsing
+app.use(express.json());
+
+// middleware - API routes
+
+// listen
+app.listen(port, () => console.log(`Server is running on port ${port}`));
